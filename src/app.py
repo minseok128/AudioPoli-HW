1e7314de8cebee6ba53553f9439d3001205b989a
minseok128 <minseok128128@gmail.com>
2024-01-28 21:21:44 +0900

    Update app : 주석 추가

--- src/app.py
+++ src/app.py
@@ -32,7 +32,7 @@ def encode_audio(file_path):
     return encoded_audio
 
 def post_to_server(filename):
-    print("서버로 전송 시도")
+    # 서버로 전송 시도
     data = {
             'id': PI_ID(), 
             'date': time.strftime('%Y-%m-%d'),
@@ -44,18 +44,16 @@ def post_to_server(filename):
     response = requests.post('http://localhost:3000/rasberry', data=data)
 
 def sound_pressure_level(signal):
-    """ Calculate the sound pressure level of the signal """
     rms = np.sqrt(np.mean(signal**2))
     spl = 20 * np.log10(rms / 20e-6)  # Sound Pressure Level in dB
     return spl
 
 def record_audio(duration, fs, filename):
-    """ Record audio for a given duration and save it to a file """
-    print("녹음 시작...")
+    print("recording start")
     recording = sd.rec(int(duration * fs), samplerate=fs, channels=1)
     sd.wait()  # Wait until recording is finished
     write(filename, fs, recording)  # Save the recording
-    print(f"녹음 완료: {filename}")
+    print(f"recording deon: {filename}")
     post_to_server(filename)
 
 def main():
@@ -65,18 +63,17 @@ def main():
 
     try:
         while True:
-            print("데시벨 측정 중...")
             recording = sd.rec(int(fs * 2), samplerate=fs, channels=1, dtype='float64')
             sd.wait()
             spl = sound_pressure_level(recording[:,0])
-            print(f"현재 데시벨: {spl:.2f} dB")
+            print(f"now: {spl:.2f} dB")
 
             if spl > threshold:
                 filename = f"{PI_ID()}_{time.strftime('%Y%m%d_%H%M%S')}.wav"
                 record_audio(duration, fs, filename)
 
     except KeyboardInterrupt:
-        print("프로그램 종료")
+        print("exit")
 
 if __name__ == "__main__":
     main()
